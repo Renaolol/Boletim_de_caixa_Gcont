a637cf1d927fe3ea49a798ade03589f8e890346c
Renaolol <renanschmeier91@gmail.com>
2025-08-26 16:29:26 -0300

    Criado uma parte inicial do código

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
 import streamlit as st
 from config_pag import get_logo, set_background
-
+import pandas as pd
 
 st.set_page_config(layout='wide')
 
 get_logo()
 set_background()
 st.title("Boletim de Caixa Online Gcont")
+options_select = ["Caixa","Sicoob","Sicredi","Bradesco"]
+select = st.segmented_control("Selecione a devida conta para receber a movimentação",options=options_select)
+hist_padrao = ["Pagamento NF-e Num ", "Pagamento NFC-e "]
+col1,col2 = st.columns(2)
+with col1:
+    if select == "Caixa":
+        sub_col1,sub_col2 = st.columns(2)
+        with sub_col1:
+            valor=st.number_input("Digite o valor do lançamento: ")
+            num_doc = st.number_input("Digite o número do documento")
+        with sub_col2:    
+            hist = st.selectbox("Selecione o Histórico Padrão: ",options=hist_padrao)
+            comp_hist = st.text_input("Complemento de Histórico")
+    tipo_lancto=st.radio("Selecione o tipo de lançamento", options=["Entrada","Saída"])        
+    st.button("Salvar Lançamento")
+
+with col2:
+    pass
