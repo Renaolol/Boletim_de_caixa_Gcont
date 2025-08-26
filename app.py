7954afcdaedf77ee7f3daa3327914b85830895a7
Renaolol <renanschmeier91@gmail.com>
2025-08-26 15:43:14 -0300

    Criado o Config_pag.py e adicionado logo e background

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+from config_pag import get_logo, set_background
+
 
 st.set_page_config(layout='wide')
 
+get_logo()
+set_background()
 st.title("Boletim de Caixa Online Gcont")
