e57792fb700f66961eb36c6b22b58a065fecc1ec
Renaolol <renanschmeier91@gmail.com>
2025-09-26 17:41:06 -0300

    Ajustes

--- dependencies.py
+++ dependencies.py
@@ -52,8 +52,81 @@ def get_clientes():
     conn= conecta_banco()
     cursor = conn.cursor()
     query="""
-    SELECT nome
+    SELECT nome, codigo
     FROM clientes
     """
     cursor.execute(query, )
-    cursor.fetchall
+    clientes = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return clientes
+
+def cadastra_clientes (nome,codigo):
+    conn = conecta_banco()
+    cursor = conn.cursor()
+    query="""
+    INSERT INTO clientes(nome,codigo)
+    VALUES(%s,%s)
+"""
+    cursor.execute(query, (nome,codigo))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    return("Cliente cadastrado")
+
+
+def cadastra_historico (cliente,descricao,cod_conta):
+    conn=conecta_banco()
+    cursor = conn.cursor()
+    query = """
+        INSERT INTO historicos(cliente, descricao, cod_conta)
+        VALUES (%s,%s,%s)
+        """
+    cursor.execute(query, (cliente,descricao,cod_conta))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return
+
+def get_historicos(empresa):
+    conn = conecta_banco()
+    cursor = conn.cursor()
+    query="""
+        SELECT cliente, descricao, cod_conta
+        FROM historicos
+        WHERE cliente = %s
+    """
+    cursor.execute(query, (empresa,))
+    historicos = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return historicos  
+
+def create_conta(empresa, conta, cod_contabil, tipo):
+    conn = conecta_banco()
+    cursor = conn.cursor()
+    query="""
+    INSERT INTO contas(empresa,conta,cod_contabil,tipo)
+    VALUES (%s,%s,%s,%s)
+    """
+    cursor.execute(query, (empresa, conta, cod_contabil, tipo))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return
+
+def get_contas(empresa):
+    conn=conecta_banco()
+    cursor=conn.cursor()
+    query="""
+    SELECT empresa, conta,cod_contabil,tipo
+    FROM contas
+    WHERE empresa = %s
+    """  
+    cursor.execute(query, (empresa, ))
+    contas = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return contas
+
--- pages/admin.py
+++ pages/admin.py
@@ -1,7 +1,55 @@
 import streamlit as st
+from time import sleep
 from config_pag import set_background, get_logo
-
-
+from dependencies import cadastra_clientes, conecta_banco ,get_clientes, get_historicos, cadastra_historico,create_conta,get_contas
 get_logo()
 set_background()
 st.title("Página de Administração")
+col_cadastro, col_clientes = st.columns(2)
+with col_cadastro:
+    nome= st.text_input("Insira o nome do cliente a ser cadastrado: ",width=300)
+    cod = st.number_input("Insira o código do cliente disponível na domínio: ",width=300)
+    salvar = st.button("Salvar Cliente")    
+    if salvar:
+        cadastra_clientes(nome,cod)
+        st.success("Cliente salvo com sucesso!")
+        sleep(1)
+        st.rerun()
+
+    empresa = st.text_input("Informe o código do cliente",1,width=300)
+with col_clientes:
+    clientes=get_clientes()
+    st.dataframe(clientes)    
+st.divider()
+col1,col2 = st.columns(2)
+with col1:
+    historicos = get_historicos(empresa)
+    st.dataframe(historicos)
+with col2:
+    desc_historico = st.text_area("Informe a descrição do histórico: ")
+    cod_conta = st.text_input("Informe o código contabil da conta")
+
+    slv_hist = st.button("Salvar Histórico")
+    if slv_hist:
+        cadastra_historico(empresa,desc_historico,cod_conta)
+        st.success("Histórico Salvo com sucesso!")
+        sleep(1)
+        st.rerun()    
+
+st.divider()
+col3,col4 = st.columns(2)
+with col3:
+    nome_conta = st.text_input("Insira o nome da conta desejada: ")
+    cod_contabil = st.text_input("Insira o código da conta: ")
+    tipo = st.selectbox("Selecione o tipo da conta:",options=["Receita","Despesas"])
+    salvar_conta = st.button("Salvar conta")
+    if salvar_conta:
+        create_conta(empresa, nome_conta, cod_contabil, tipo)
+        st.success("Conta criada com sucesso!")
+        sleep(1)
+        st.rerun()
+with col4:
+    contas_contabeis=get_contas(empresa)
+    st.dataframe(contas_contabeis)
+
+st.divider()    
