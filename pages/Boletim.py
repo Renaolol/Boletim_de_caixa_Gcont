5c40bb28b269adb1782d1acedfe60104351e8dab
Renaolol <renanschmeier91@gmail.com>
2025-11-28 09:01:11 -0300

    Update Boletim.py

--- pages/Boletim.py
+++ pages/Boletim.py
@@ -27,10 +27,20 @@ contas_por_codigo = dict(zip(conta_df["Cod_contabil"], conta_df["Conta"]))
 st.title(f"Boletim de caixa online - GCONT - {st.session_state.get('name')}")
 st.divider()
 portadores = pd.DataFrame(get_portador(empresa),columns=["id","empresa","nome_conta","cod_contabil"])
+portador_labels = dict(zip(portadores["cod_contabil"], portadores["nome_conta"]))
 if empresa == "3":
-    portador_select = st.selectbox("Selecione a conta a ser utilizada",options=(portadores["cod_contabil"]+portadores["nome_conta"]))
+    portador_select = st.selectbox(
+        "Selecione a conta a ser utilizada",
+        options=portadores["cod_contabil"],
+        format_func=lambda codigo: f"{codigo} - {portador_labels.get(codigo, '')}",
+    )
 else:    
-    portador_select = st.radio("Selecione a conta a ser utilizada",options=portadores["cod_contabil"],horizontal=True,captions=portadores["nome_conta"])
+    portador_select = st.radio(
+        "Selecione a conta a ser utilizada",
+        options=portadores["cod_contabil"],
+        horizontal=True,
+        format_func=lambda codigo: f"{codigo} - {portador_labels.get(codigo, '')}",
+    )
 col1,col2 = st.columns([1.5,3.5])
 with col1:
     st.subheader("Novo lançamento")
